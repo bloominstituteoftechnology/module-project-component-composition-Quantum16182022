71ce367f9fda73ae0ef8bae5b096eaf8ee317469
Stanley kemboi <kstan8482@gmail.com>
2024-06-26 13:39:16 -0500

    w-done

---
+++ frontend/components/Figure.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Figure({imageURL, caption }){
+
+    return (
+                
+    <Figure>
+        <imag src={imageURL}/>
+        <figcaption>Awesome pic taken on {caption}</figcaption>
+
+    </Figure>
+)
+}
